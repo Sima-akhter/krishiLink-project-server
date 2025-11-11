3f83c8b5306b51de2477ef60f37a6d3a88fa1e29
Sima-Akther <saimaislambdk@gmail.com>
2025-11-11 21:12:30 +0600

    data put added

--- index.js
+++ index.js
@@ -13,51 +13,78 @@ const uri = "mongodb+srv://krishiLink:DQx9dJnoYmoT7TiA@programming-hero.ifoutmp.
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-   const db = client.db('krishiLink-project');
-   const krishiLinkCollection = db.collection('krishiLink');
+        const db = client.db('krishiLink-project');
+        const krishiLinkCollection = db.collection('krishiLink');
 
-   app.get('/krishiLink', async(req, res)=>{
-   const result =await krishiLinkCollection.find().toArray()
-    res.send(result)
-   });
+        app.get('/krishiLink', async (req, res) => {
+            const result = await krishiLinkCollection.find().toArray()
+            res.send(result)
+        });
 
-    app.get('/krishiLink/:id', async(req, res)=>{
-        const {id} = req.params
-        console.log(id)
-        const result =await krishiLinkCollection.findOne({_id: new ObjectId(id)})
-        res.send({
-            success: true,
-            result
+        app.get('/krishiLink/:id', async (req, res) => {
+            const { id } = req.params
+            console.log(id)
+            const result = await krishiLinkCollection.findOne({ _id: new ObjectId(id) })
+            res.send({
+                success: true,
+                result
+            })
         })
-    })
 
-   app.post('/krishiLink', async(req, res)=>{
-    const data = req.body
-    const result = await krishiLinkCollection.insertOne(data)
-    res.send({
-        success:true,
-        result
-    })
-   });
+        app.post('/krishiLink', async (req, res) => {
+            const data = req.body
+            const newKrishi = { ...data, create_at: new Date() }
+            const result = await krishiLinkCollection.insertOne(newKrishi)
+            res.send({
+                success: true,
+                result
+            })
+        });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+        app.put('/krishiLink/:id', async (req, res) => {
+            const { id } = req.params
+            const filter = { _id: new ObjectId(id) }
+            const updateKrishiLink = req.body
+
+            const update = {
+                $set: updateKrishiLink
+
+            }
+            const result = await krishiLinkCollection.updateOne(filter, update)
+            res.send(result)
+
+        })
+
+        app.get('/latestKrishi', async (req, res) => {
+            const result = await krishiLinkCollection.find().sort({ create_at: -1 }).limit(6).toArray()
+            res.send(result)
+        })
+
+        app.get('/my-posted', async (req, res) => {
+            const email = req.query.email
+            const result = await krishiLinkCollection.find({'owner.ownerEmail':email}).toArray()
+            res.send(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -65,11 +92,11 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
 
 
